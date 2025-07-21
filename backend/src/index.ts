cbd184c8397beebb75e0a502e644576eb201d908
vanekt <vanekt@yandex.ru>
2025-07-21 11:16:01 -0300

    backend set host

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,7 @@ const start = async () => {
   }
 
   try {
-    await fastify.listen({ port });
+    await fastify.listen({ port, host: "0.0.0.0" });
   } catch (err) {
     console.error(err);
     process.exit(1);
