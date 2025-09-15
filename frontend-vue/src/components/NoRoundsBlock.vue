2bfeb1dedc0c16ba83871369fa0ec59df803e0df
vanekt <vanekt@yandex.ru>
2025-09-15 09:35:44 -0300

    rm

--- frontend-vue/src/components/NoRoundsBlock.vue
+++ frontend-vue/src/components/NoRoundsBlock.vue
@@ -6,7 +6,6 @@ const authStore = useAuthStore();
 <template>
   <div class="py-12 text-center">
     <div class="text-lg text-gray-400">Раундов пока нет</div>
-    const authStore = useAuthStore();
     <p v-if="authStore.isAdmin" class="mt-2 text-gray-500">
       Создайте первый раунд
     </p>
